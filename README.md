ba2991553a72e774f54f17b74cff8dd34dbfc758
Astramast <azerqsdfazerqsdg@gmail.com>
2024-11-04 22:20:47 +0100

    Final version of main installation script

---
+++ README.md
@@ -0,0 +1,19 @@
+# HAL2
+Hal2 est le serveur central du hs dans le hs pour le hs.
+L'idée de cette nouvelle version est d'avoir une architecture en plein de petits services facile à modifier, afin de rendre HAL modulable.
+Pour ce faire on a créé un script d'installation à lancer pour installer HAL2.
+L'architecture de ce repo d'installation d'HAL2 est la suivante :
+-README.md # ce fichier
+-hal2.sh   # le fichier d'installation principal
+-programs/ # Dossier contenant les scripts d'installation de chaque module
+--a.sh     # Script d'intallation du module a
+
+Les scripts d'installation doivent suivre les propriétés suivantes :
+- Être exécutable
+- Les deux premières lignes doivent êtres les suivantes :
+
+`#!/bin/bash`
+`#Descripteur du module`
+
+Note : Il est préférable d'avoir un script d'installation du module dans le repo du module, de cette manière le script d'installation dans ce repo se contente de clone le module au bon endroit, lancer le script d'installation et cleaner si besoin est.
+
--- hal2.sh
+++ hal2.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
+DEVMODE=0
+
 if [ "$EUID" -ne 0 ]; then
 	echo "Please run script with sudo."
 	exit 1
+fi
+
+programs=($(ls ./programs/ | cut -d'.' -f 1))
+
+# Descriptor is commented in the second line of the file
+declare -A programs_descriptors
+for ((i = 0; i < ${#programs[@]}; i++)); do
+	programs_descriptors[${programs[$i]}]=$(sed '2q;d' ./programs/${programs[$i]}.sh | sed 's/^.//')
+
+done
+
+if [ ${#programs[@]} -ne ${#programs_descriptors[@]}  ]; then
+	echo "Error : Number of programs do not match number of programs descriptors."
+	echo "Installation script is misconfigured. Exiting..."
+	exit 1
+fi
+
+
+echo "Programs available to installation : "
+for ((i = 0; i < ${#programs_descriptors[@]}; i++)); do
+        echo "$i) ${programs_descriptors[${programs[$i]}]}"
+done
+
+read -p "Enter program numbers to install (separated by space) [0]: " -r programs_to_install
+if [ -z "$programs_to_install" ]; then
+	programs_to_install=0
+fi
+
+if [[ $programs_to_install =~ [^0-9\ ] ]]; then
+	echo "Invalid input. Exiting..."
+	exit 1
+fi
+
+if [[ $DEVMODE -eq 1 ]]; then
+	echo "Exiting because dev mode is on."
+	exit 0
 fi
 
 sudo apt update && sudo apt full-upgrade
@@ -17,5 +55,15 @@ cp config-files/.bashrc ~/.bashrc
 cp config-files/.bash_aliases ~/.bash_aliases
 rm -rf config-files
 
-
+if [[ $programs_to_install =~ "0" ]]; then
+	for ((i = 0; i < ${#programs[@]}; i++)); do
+		echo "Installing ${programs_descriptors[${programs[$i]}]}..."
+		./programs/${programs[$i]}.sh
+	done
+else
+	for program in $programs_to_install; do
+		echo "Installing ${programs_descriptors[${programs[$program]}]}..."
+		./programs/${programs[$program]}.sh
+	done
+fi
 
