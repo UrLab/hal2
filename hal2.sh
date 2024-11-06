28e3b21ad3deea22b4077a5f9e9da9c18bb263d1
Astramast <azerqsdfazerqsdg@gmail.com>
2024-11-06 20:53:31 +0100

    armv7

--- hal2.sh
+++ hal2.sh
@@ -67,7 +67,7 @@ separator
 echo "Portainer installation..."
 separator
 sudo apt install -y curl docker.io docker-compose
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-armv7 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo usermod -aG docker $USER
