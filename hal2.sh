4a5b6af83805b9c74bae44b45fc005cfe4d59387
Astramast <azerqsdfazerqsdg@gmail.com>
2024-11-06 19:48:53 +0100

    added explicity of what being done

--- hal2.sh
+++ hal2.sh
@@ -7,9 +7,20 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 
+separator() {
+    echo "$(printf '%.0s-' {1..40})"
+}
+
+
+separator
+echo "Update and Git installation..."
+separator
 sudo apt update
 sudo apt install git -y
 
+separator
+echo "Raspifresh installation..."
+separator
 read -p "Run Raspifresh installation ? (see https://github.com/urlab/raspifresh) [y/N]: " -n 1 -r reply
 if [[ $reply =~ ^[Yy]$ ]]; then
 	echo "\nExecuting Raspifresh..."
@@ -25,6 +36,9 @@ if [[ -n $reply && ! $reply =~ ^[Nn]$ ]]; then
 	exit 1
 fi
 
+separator
+echo "Programs selection..."
+separator
 programs=($(ls ./programs/ | grep "\.sh"))
 
 # Descriptor is commented in the second line of the file
@@ -53,7 +67,11 @@ if [[ $DEVMODE -eq 1 ]]; then
 	exit 0
 fi
 
-sudo apt update && sudo apt install -y curl docker.io docker-compose
+
+separator
+echo "Portainer installation..."
+separator
+sudo apt install -y curl docker.io docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
@@ -61,12 +79,16 @@ sudo usermod -aG docker $USER
 
 if [[ $programs_to_install =~ "0" ]]; then
 	for ((i = 0; i < ${#programs[@]}; i++)); do
+		separator
 		echo "Installing ${programs_descriptors[$i]}..."
+		separator
 		./programs/${programs[$i]}
 	done
 else
 	for i in $programs_to_install; do
+		separator
 		echo "Installing ${programs_descriptors[$i]}..."
+		separator
 		./programs/${programs[$i]}
 	done
 fi
