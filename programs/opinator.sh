53bf7832a3ac519f45a7311863f176d6d86e9858
Astramast <azerqsdfazerqsdg@gmail.com>
2025-03-06 01:50:26 +0100

    updated .env entries query for opinator

--- programs/opinator.sh
+++ programs/opinator.sh
@@ -3,5 +3,28 @@
 
 git clone https://github.com/urlab/opinator.git $HOME/opinator/
 cd $HOME/opinator/software/
+
+echo "Need entries for .env opinator triggers"
+echo "For the Api Key, nothing will be written in terminal"
+read -r -s -p "Enter opinator API Key: " ApiKey
+echo "API_KEY=$ApiKey" >> .env
+unset ApiKey
+
+read -r -p "Enter opinator Server URL : " ServerUrl
+echo "SERVER_URL=$ServerUrl" >> .env
+unset ServerUrl
+
+read -r -p "Enter opinator WebHook URL : " WebHookUrl
+echo "WEBHOOK_URL=$WebHookUrl" >> .env
+unset WebHookUrl
+
+read -r -p "Enter opinator MQTT Broker Ip : " MqttBrokerUrl
+echo "MQTT_HOST=$MqttBrokerUrl" >> .env
+unset MqttBrokerUrl
+
+read -r -p "Enter opinator MQTT Topic : " MqttTopic
+echo "OPINATOR_TOPIC=$MqttTopic" >> .env
+unset MqttTopic
+
 docker-compose up -d
 
